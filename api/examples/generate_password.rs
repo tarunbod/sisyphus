75cda4a208654687a069f9fbfa07f17845bc900e
Tarun Boddupalli <tarunbod@gmail.com>
2023-03-03 22:48:49 -0500

    Get parity with the actual tool lol

--- api/examples/generate_password.rs
+++ api/examples/generate_password.rs
@@ -1,6 +1,6 @@
 use api::{generate_site_password, PasswordType};
 
 fn main() {
-    let password = generate_site_password("banana colored duckling", "Robert Lee Mitchell", "twitter.com", 1, PasswordType::Long);
+    let password = generate_site_password("pink fluffy door frame", "Robert Lee Mitchell", "apple.com", 1, PasswordType::Long);
     println!("{password}");
 }
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,22 +1,26 @@
+#![allow(non_upper_case_globals)] // FIXME
+
 type MasterKey = [u8; 64];
 type TemplateSeed = [u8; 32];
 
-const TEMPLATE_GROUP_V: &'static str = "AEIOU";
-const TEMPLATE_GROUP_C: &'static str = "BCDFGHJKLMNPQRSTVWXYZ";
-const TEMPLATE_GROUP_v: &'static str = "aeiou";
-const TEMPLATE_GROUP_c: &'static str = "bcdfghjklmnpqrstvwxyz";
-const TEMPLATE_GROUP_A: &'static str = "AEIOUBCDFGHJKLMNPQRSTVWXYZ";
-const TEMPLATE_GROUP_a: &'static str = "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
-const TEMPLATE_GROUP_n: &'static str = "0123456789";
-const TEMPLATE_GROUP_o: &'static str = "@&%?,=[]_:-+*$#!'^~;()/.";
-const TEMPLATE_GROUP_x: &'static str = "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz0123456789!@#$%^&*()";
+const TEMPLATE_GROUP_V: &str = "AEIOU";
+const TEMPLATE_GROUP_C: &str = "BCDFGHJKLMNPQRSTVWXYZ";
+const TEMPLATE_GROUP_v: &str = "aeiou";
+const TEMPLATE_GROUP_c: &str = "bcdfghjklmnpqrstvwxyz";
+const TEMPLATE_GROUP_A: &str = "AEIOUBCDFGHJKLMNPQRSTVWXYZ";
+const TEMPLATE_GROUP_a: &str = "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
+const TEMPLATE_GROUP_n: &str = "0123456789";
+const TEMPLATE_GROUP_o: &str = "@&%?,=[]_:-+*$#!'^~;()/.";
+const TEMPLATE_GROUP_x: &str = "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz0123456789!@#$%^&*()";
 
 fn generate_master_key<S: AsRef<str>>(master_password: S, name: S) -> MasterKey {
     let mut output = [0; 64];
-    let salt = format!("com.lyndir.masterpassword{}{}", name.as_ref().len(), name.as_ref());
+    let mut salt_bytes = b"com.lyndir.masterpassword".to_vec();
+    salt_bytes.extend_from_slice(&(name.as_ref().len() as u32).to_be_bytes());
+    salt_bytes.extend_from_slice(name.as_ref().as_bytes());
     scrypt::scrypt(
         master_password.as_ref().as_bytes(), // password
-        salt.as_bytes(),
+        &salt_bytes,
         &scrypt::Params::new(15, 8, 2).unwrap(),
         &mut output
     ).unwrap();
@@ -28,9 +32,13 @@ fn generate_template_seed<S: AsRef<str>>(master_key: MasterKey, site_name: S, si
 
     type HmacSha256 = hmac::Hmac<sha2::Sha256>;
     let mut mac = HmacSha256::new_from_slice(&master_key).unwrap();
-    let message = format!("com.lyndir.masterpassword{}{}{}", site_name.as_ref().len(), site_name.as_ref(), site_counter);
-    mac.update(message.as_bytes());
-    mac.finalize().into_bytes().into()
+    let mut message_bytes = b"com.lyndir.masterpassword".to_vec();
+    message_bytes.extend_from_slice(&(site_name.as_ref().len() as u32).to_be_bytes());
+    message_bytes.extend_from_slice(site_name.as_ref().as_bytes());
+    message_bytes.extend_from_slice(&site_counter.to_be_bytes());
+    mac.update(&message_bytes);
+    let output = mac.finalize().into_bytes().into();
+    output
 }
 
 pub enum PasswordType {
@@ -44,39 +52,15 @@ pub enum PasswordType {
 
 impl PasswordType {
     fn get_template(&self, seed: TemplateSeed) -> &'static str {
-        match self {
-            Self::MaximumSecurity => ["anoxxxxxxxxxxxxxxxxx", "axxxxxxxxxxxxxxxxxno"][(seed[0] % 2) as usize],
-            Self::Long => {
-                let templates = [
-                    "CvcvnoCvcvCvcv",
-                    "CvcvCvcvnoCvcv",
-                    "CvcvCvcvCvcvno",
-                    "CvccnoCvcvCvcv",
-                    "CvccCvcvnoCvcv",
-                    "CvccCvcvCvcvno",
-                    "CvcvnoCvccCvcv",
-                    "CvcvCvccnoCvcv",
-                    "CvcvCvccCvcvno",
-                    "CvcvnoCvcvCvcc",
-                    "CvcvCvcvnoCvcc",
-                    "CvcvCvcvCvccno",
-                    "CvccnoCvccCvcv",
-                    "CvccCvccnoCvcv",
-                    "CvccCvccCvcvno",
-                    "CvcvnoCvccCvcc",
-                    "CvcvCvccnoCvcc",
-                    "CvcvCvccCvccno",
-                    "CvccnoCvcvCvcc",
-                    "CvccCvcvnoCvcc",
-                    "CvccCvcvCvccno",
-                ];
-                templates[(seed[0] % 21) as usize]
-            },
-            Self::Medium => ["CvcnoCvc", "CvcCvcno"][(seed[0] % 2) as usize],
-            Self::Short => "Cvcn",
-            Self::Basic => ["aaanaaan", "aannaaan", "aaannaaa"][(seed[0] % 3) as usize],
-            Self::PIN => "nnnn"
-        }
+        let templates: &[&str] = match self {
+            Self::MaximumSecurity => &["anoxxxxxxxxxxxxxxxxx", "axxxxxxxxxxxxxxxxxno"],
+            Self::Long => &["CvcvnoCvcvCvcv", "CvcvCvcvnoCvcv", "CvcvCvcvCvcvno", "CvccnoCvcvCvcv", "CvccCvcvnoCvcv", "CvccCvcvCvcvno", "CvcvnoCvccCvcv", "CvcvCvccnoCvcv", "CvcvCvccCvcvno", "CvcvnoCvcvCvcc", "CvcvCvcvnoCvcc", "CvcvCvcvCvccno", "CvccnoCvccCvcv", "CvccCvccnoCvcv", "CvccCvccCvcvno", "CvcvnoCvccCvcc", "CvcvCvccnoCvcc", "CvcvCvccCvccno", "CvccnoCvcvCvcc", "CvccCvcvnoCvcc", "CvccCvcvCvccno"],
+            Self::Medium => &["CvcnoCvc", "CvcCvcno"],
+            Self::Short => &["Cvcn"],
+            Self::Basic => &["aaanaaan", "aannaaan", "aaannaaa"],
+            Self::PIN => &["nnnn"]
+        };
+        templates[(seed[0] as usize % templates.len()) as usize]
     }
 }
 
@@ -87,19 +71,18 @@ pub fn generate_site_password<S: AsRef<str>>(master_password: S, name: S, site_n
     let mut password = String::new();
     for (i, c) in template.chars().enumerate() {
         let password_char = match c {
-            'V' => TEMPLATE_GROUP_V.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_V.len()) as usize],
-            'C' => TEMPLATE_GROUP_C.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_C.len()) as usize],
-            'v' => TEMPLATE_GROUP_v.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_v.len()) as usize],
-            'c' => TEMPLATE_GROUP_c.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_c.len()) as usize],
-            'A' => TEMPLATE_GROUP_A.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_A.len()) as usize],
-            'a' => TEMPLATE_GROUP_a.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_a.len()) as usize],
-            'n' => TEMPLATE_GROUP_n.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_n.len()) as usize],
-            'o' => TEMPLATE_GROUP_o.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_o.len()) as usize],
-            'x' => TEMPLATE_GROUP_x.as_bytes()[(template_seed[i + 1] as usize % TEMPLATE_GROUP_x.len()) as usize],
+            'V' => TEMPLATE_GROUP_V.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_V.len()],
+            'C' => TEMPLATE_GROUP_C.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_C.len()],
+            'v' => TEMPLATE_GROUP_v.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_v.len()],
+            'c' => TEMPLATE_GROUP_c.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_c.len()],
+            'A' => TEMPLATE_GROUP_A.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_A.len()],
+            'a' => TEMPLATE_GROUP_a.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_a.len()],
+            'n' => TEMPLATE_GROUP_n.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_n.len()],
+            'o' => TEMPLATE_GROUP_o.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_o.len()],
+            'x' => TEMPLATE_GROUP_x.as_bytes()[template_seed[i + 1] as usize % TEMPLATE_GROUP_x.len()],
             _ => continue
         };
         password.push(password_char as char);
     }
-    println!("Done.");
     password
 }
