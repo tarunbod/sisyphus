1c636e4b466dfbb20e75f84df95542d7d7d13bec
Tarun Boddupalli <tarunbod@gmail.com>
2023-03-04 17:44:51 -0500

    we got subcommands in this bitch now

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,25 +1,19 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use arboard::Clipboard;
 use sisyphus_api::{
     generate_site_password,
     PasswordType
 };
 
-#[derive(Parser, Debug)]
-#[command(version, about = "CLI for sisyphus password manager.")]
+#[derive(Parser)]
+#[command(version)]
+#[command(about = "CLI for sisyphus password manager.")]
 struct Args {
-    username: String,
-
-    site_name: String,
-
-    #[arg(default_value_t = 1)]
-    counter: u32,
-
-    #[arg(value_enum, default_value_t = ArgPasswordType::Long)]
-    password_type: ArgPasswordType,
+    #[command(subcommand)]
+    command: Command
 }
 
-#[derive(Clone, Debug, clap::ValueEnum)]
+#[derive(Clone, clap::ValueEnum)]
 enum ArgPasswordType {
     Max,
     Long,
@@ -27,6 +21,23 @@ enum ArgPasswordType {
     Short,
     Basic,
     PIN
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Get {
+        site_name: String,
+
+        #[arg(default_value_t = 1)]
+        counter: u32,
+
+        #[arg(value_enum, default_value_t = ArgPasswordType::Long)]
+        password_type: ArgPasswordType,
+    },
+
+    SetUser {
+        username: String
+    }
 }
 
 impl Into<PasswordType> for ArgPasswordType {
@@ -44,8 +55,26 @@ impl Into<PasswordType> for ArgPasswordType {
 
 fn main() {
     let args = Args::parse();
+    match args.command {
+        Command::Get { site_name, counter, password_type} => get_password(site_name, counter, password_type.into()),
+        Command::SetUser { username } => todo!()
+    }
+}
+
+fn get_username() -> Option<String> {
+    Some("Tarun Boddupalli".into())
+}
+
+fn get_password(site_name: String, counter: u32, password_type: PasswordType) {
+    let username = match get_username() {
+        Some(name) => name,
+        None => {
+            println!("No username found. Please set your username with `spm set-user` first.");
+            return;
+        }
+    };
     let user_key = rpassword::prompt_password("Enter your master password: ").unwrap();
-    let password = generate_site_password(args.username, user_key, args.site_name, args.counter, args.password_type.into());
+    let password = generate_site_password(username, user_key, site_name, counter, password_type);
     let mut clipboard = Clipboard::new().unwrap();
     clipboard.set_text(password).unwrap();
     println!("Password copied to clipboard");
